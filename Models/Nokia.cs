8328a82584366707ccaf28b44c37065be3584f58
CassianoCardoso <dj_kassiano@hotmail.com>
2024-01-08 20:03:05 -0300

    [200~

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+
+    public class Nokia : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando ... {nomeApp}");
+            Console.WriteLine("Em progresso...");
+            Console.WriteLine("Instalação concluida.");
+        }
+
+        internal void InstalarApp(string v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
+internal class Program
+{
+  private static void Main(string[] args)
+  {
+    Nokia nokia = new Nokia("32998709771", "Tijolao", "123456cfder", 8);
+
+    nokia.Ligar();
+    nokia.ReceberLigacao();
+    nokia.InstalarApp("Whatsapp");
+
+    Iphone iphone = new Iphone("198765243", "AppleSmart", "imei2132", 16);
+
+    iphone.ReceberLigacao();
+  }
+}
+
+
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
